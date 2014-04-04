d1ff91cb9194e8f6f9fe4ac5b2be082a0a2035ae
Ryan Case <rmcase@gmail.com>
2014-04-04 10:49:18 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TankCommander::Application.config.secret_key_base = '33a64951ae18ce2ccbba41b5afdb8fd55e63e0b0f8506edce2f274791e8199571823620ecb814d26cccf81ed686370c532b0867e338c4c51c26b3cf6aca97681'
